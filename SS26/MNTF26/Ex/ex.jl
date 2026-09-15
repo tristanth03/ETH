7b5612331d29d0467439e18f33adb09e71169ec2
tristanth03 <tristanthordarson@gmail.com>
2026-09-15 15:45:11 +0200

    Bck

---
+++ SS26/MNTF26/Ex/ex.jl
@@ -0,0 +1,12 @@
+# this will render ? $f_{\theta}$
+# and this? \(f_{\theta}\)
+"""
+Compute ``f_\\theta(x) = x^\\theta``.
+"""
+
+@doc raw"""
+Compute ``f_\theta(x) = x^\theta``.
+""" f
+
+f(x, θ) = x^θ
+
